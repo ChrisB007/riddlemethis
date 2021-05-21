23d1b3e361fce96f71b92ca879d60e57feb6bf91
ChrisB007 <chrisbitoy@gmail.com>
2021-05-21 13:39:03 -0400

    style update on the grid

--- src/components/Riddles.js
+++ src/components/Riddles.js
@@ -9,7 +9,7 @@ const Riddles = props => {
             <Row>
                 <Col className="riddlegrid" xs={12} sm={12} md={4}>
                 <Accordion>
-                <Card>
+                <Card id="easy">
                     <Card.Header>
                     <Accordion.Toggle as={Button} variant="link" eventKey="0">
                         Easy Riddles
@@ -21,23 +21,23 @@ const Riddles = props => {
                 </Card>
                 </Accordion> 
                 </Col>
-                <Col className="riddlegrid" xs={12} sm={12} md={4}>
+                <Col  className="riddlegrid" xs={12} sm={12} md={4}>
                 <Accordion>
-                <Card>
+                <Card id="medium">
                     <Card.Header>
                     <Accordion.Toggle as={Button} variant="link" eventKey="0">
                         Intermediate Riddles
                     </Accordion.Toggle>
                     </Card.Header>
                     <Accordion.Collapse eventKey="0">
-                    <Card.Body>Hello! I'm the body</Card.Body>
+                    <Card.Body>Hello! I'm the bodyHello! I'm the bodyHello! I'm the bodyHello! I'm the bodyHello! I'm the body</Card.Body>
                     </Accordion.Collapse>
                 </Card>
                 </Accordion> 
                 </Col>
                 <Col className="riddlegrid" xs={12} sm={12} md={4}>
                 <Accordion>
-                <Card>
+                <Card id="hard">
                     <Card.Header>
                     <Accordion.Toggle as={Button} variant="link" eventKey="0">
                         Hard Riddles
