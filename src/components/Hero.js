42757d9ed31c81ed698fa74e9b7df9fb1ac2339c
ChrisB007 <chrisbitoy@gmail.com>
2021-05-21 13:53:49 -0400

    updates to the hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Jumbotron, Button} from 'react-bootstrap';
+import { Container, Row, Col, Jumbotron, Accordion, Card, Button} from 'react-bootstrap';
 
 const Hero = () => {
     return (
@@ -8,7 +8,21 @@ const Hero = () => {
             <Row>
                 <Col>
                 <Jumbotron className="jumbo">
-                <h1 className="heydev fontsfam">Use me well, keep me clean, and I’ll never tell what I have seen...What am I?</h1>
+                    <div>
+                        <h1 className="heydev fontsfam">Use me well, keep me clean, and I’ll never tell what I have seen...What am I?</h1>
+                        <Accordion>
+                        <Card id="heroanswer">
+                            <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                Check the answer
+                            </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                            <Card.Body>A Toilet</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        </Accordion> 
+                    </div>
                     <hr className="my-2" />
                     <p className="lead fonts">RiddleMeThis is a free well formatted REST API that serves interesting, and yes funny riddles.</p>
                     <p className="lead fonts"></p>
